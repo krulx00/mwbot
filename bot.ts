fe52ee322dc5b6a4991931dee7666a744126c86a
haerulysin <17102039@ittelkom-pwt.ac.id>
2024-02-26 01:21:35 +0700

    ss

--- bot.ts
+++ bot.ts
@@ -6,7 +6,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const token = process.env.TELEGRAM_TOKEN || '6279481409:AAHzhmO2V7Ljqhx-R2s5t27elr7AvSqtgLM';
-const APIServerURL = process.env.API_SERVER || 'https://s1.mdkruls.net/storygenerator';
+const APIServerURL = process.env.API_SERVER || 'http://s1.mdkruls.net/storygenerator';
 
 const bot = new Telegraf(token);
 const telegram = new Telegram(token);
