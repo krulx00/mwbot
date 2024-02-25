ea4b373de20b1e154aa96e792c64aa666f2a1061
haerulysin <17102039@ittelkom-pwt.ac.id>
2024-02-26 01:01:33 +0700

    Catching & Handle Error Update

--- bot.ts
+++ bot.ts
@@ -11,8 +11,11 @@ const APIServerURL =
 
 const bot = new Telegraf(token);
 const telegram = new Telegram(token);
-bot.start((ctx) => {
-  ctx.reply("Hello " + ctx.from.first_name + "!");
+
+
+
+bot.start(async(ctx) => {
+  await ctx.reply("Hello " + ctx.from.first_name + "!");
 });
 
 bot.on(message("photo"), async (ctx) => {
@@ -22,23 +25,41 @@ bot.on(message("photo"), async (ctx) => {
   const { href } = fileLink;
   const imageBuffer = await getFile(href);
   const imageResult = await getStory(imageBuffer, ctx.message.caption!);
-  ctx.replyWithDocument(Input.fromBuffer(imageResult,`${new Date().valueOf()}.png`),{caption:"Document - High Resolution"})
-  ctx.replyWithPhoto(Input.fromBuffer(imageResult,`${new Date().valueOf()}.png`),{caption:'Photo - Standard Web Resolution'})
+  await ctx.replyWithDocument(
+    Input.fromBuffer(imageResult, `${new Date().valueOf()}.png`),
+    { caption: "Document - High Resolution" }
+  );
+  await ctx.replyWithPhoto(
+    Input.fromBuffer(imageResult, `${new Date().valueOf()}.png`),
+    { caption: "Photo - Standard Web Resolution" }
+  );
+});
+
+bot.on(message("document"), async (ctx) => {
+  ctx.reply("Please wait...");
+  const fileLink = await telegram.getFileLink(ctx.message.document.file_id);
+  const { href } = fileLink;
+  const imageBuffer = await getFile(href);
+  const imageResult = await getStory(imageBuffer, ctx.message.caption!);
+  await ctx.replyWithDocument(
+    Input.fromBuffer(imageResult, `${new Date().valueOf()}.png`),
+    { caption: "Document - High Resolution" }
+  );
+  await ctx.replyWithPhoto(
+    Input.fromBuffer(imageResult, `${new Date().valueOf()}.png`),
+    { caption: "Photo - Standard Web Resolution" }
+  );
 });
 
 
-bot.on(message('document'), async(ctx) => {
-  ctx.reply("Please wait...");
-  const fileLink = await telegram.getFileLink(ctx.message.document.file_id);
-  const {href} = fileLink;
-  const imageBuffer = await getFile(href);
-  const imageResult = await getStory(imageBuffer, ctx.message.caption!);
-  ctx.replyWithDocument(Input.fromBuffer(imageResult,`${new Date().valueOf()}.png`),{caption:"Document - High Resolution"})
-  ctx.replyWithPhoto(Input.fromBuffer(imageResult,`${new Date().valueOf()}.png`),{caption:'Photo - Standard Web Resolution'})
-})
-bot.on(message("text"), (ctx) => {});
 bot.launch();
 
+bot.catch(async(err) => {
+  console.log(err);
+  process.exit(1);
+});
+// process.once("SIGINT", () => bot.stop("SIGINT"));
+// process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
 const getFile = async (url: string) => {
   const response = await axios.get(url, { responseType: "arraybuffer" });
@@ -46,7 +67,7 @@ const getFile = async (url: string) => {
 };
 const getStory = async (image: Buffer, title: string) => {
   let data = new FormData();
-  data.append("title", title || '');
+  data.append("title", title || "");
   data.append("image", image, "abcdef.jpeg");
   const resp = await axios.post(APIServerURL, data, {
     responseType: "arraybuffer",
